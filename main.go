437a0822e102da4f6ce4c71892b83816e37376e4
Layona-x <109739779+Layona-x@users.noreply.github.com>
2023-01-30 22:00:44 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+"fmt"
+)
+
+func cigkoftesosuilekirlen() {
+ var mesaj string = "Çiğ Köfteyi Elinize Sos Damlamadan Açmanın 3 Yolu Vardır: \n 1-) Peçete İle Açmayı Denemek \n 2-) Annenize Açtırmak \n 3-) Yememek Tebrikler Artık Kolunuza Sos Damlamadan Çiğ Köfte Yiyebilirsiniz" 
+  fmt.Println(mesaj)
+}
+
+func main() {
+  cigkoftesosuilekirlen() 
+}
